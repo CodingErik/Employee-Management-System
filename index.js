4726b338adeb3384b217a2ecad59aeffa7345927
Erik De Luna <codingerik1.gmail.com>
2020-07-16 13:48:16 -0500

    adding ascii art

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require('console.table');
+
+console.log(cTable.getTable); 
 
 // create the connection information for the sql database
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
 
     // Your port; if not 3306
@@ -20,12 +23,25 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
-    console.log(`sql is running`); 
+    console.log(`
+* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  * 
+|                                                                  |
+|    .--.                                                          |
+|    |__| .-------.                                                |
+|    |=.| |.-----.|                                                |
+|    |--| || EMS ||                                                |
+|    |  | |'-----'|___________________________________________     |
+|    |__|~')_____(' Welcome to The Employee Management System      |
+|                                                                  |
+|         ~ made with care by -Erik De Luna-                       |
+* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  *  
+`)
     start();
 });
 
 // function which prompts the user for what action they should take
 function start() {
+
     inquirer
         .prompt({
             name: "choice",
@@ -99,130 +115,23 @@ function update() {
                 type: "list",
                 message: "which employee would you like to update?",
                 choices: function () {
-                    return results; 
+                    return results;
                 }
             })
             .then(function (answer) {
-                console.log(answer); 
-                connection.end(); 
-            });
-    }); 
-}; 
+                connection.query("SELECT * FROM employee", function (err, results) {
 
 
-
-
-
-
-
-// function to handle posting new items up for auction
-function postAuction() {
-    // prompt for info about the item being put up for auction
-    inquirer
-        .prompt([
-            {
-                name: "item",
-                type: "input",
-                message: "What is the item you would like to submit?"
-            },
-            {
-                name: "category",
-                type: "input",
-                message: "What category would you like to place your auction in?"
-            },
-            {
-                name: "startingBid",
-                type: "input",
-                message: "What would you like your starting bid to be?",
-                validate: function (value) {
-                    if (isNaN(value) === false) {
-                        return true;
-                    }
-                    return false;
-                }
-            }
-        ])
-        .then(function (answer) {
-            // when finished prompting, insert a new item into the db with that info
-            connection.query(
-                "INSERT INTO auctions SET ?",
-                {
-                    item_name: answer.item,
-                    category: answer.category,
-                    starting_bid: answer.startingBid || 0,
-                    highest_bid: answer.startingBid || 0
-                },
-                function (err) {
-                    if (err) throw err;
-                    console.log("Your auction was created successfully!");
-                    // re-prompt the user for if they want to bid or post
-                    start();
-                }
-            );
-        });
-}
-
-function bidAuction() {
-    // query the database for all items being auctioned
-    connection.query("SELECT * FROM auctions", function (err, results) {
-        if (err) throw err;
-        // once you have the items, prompt the user for which they'd like to bid on
-        inquirer
-            .prompt([
-                {
-                    name: "choice",
-                    type: "rawlist",
-                    choices: function () {
-                        var choiceArray = [];
-                        for (var i = 0; i < results.length; i++) {
-                            choiceArray.push(results[i].item_name);
-                        }
-                        return choiceArray;
-                    },
-                    message: "What auction would you like to place a bid in?"
-                },
-                {
-                    name: "bid",
-                    type: "input",
-                    message: "How much would you like to bid?"
-                }
-            ])
-            .then(function (answer) {
-                // get the information of the chosen item
-                var chosenItem;
-                for (var i = 0; i < results.length; i++) {
-                    if (results[i].item_name === answer.choice) {
-                        chosenItem = results[i];
-                    }
-                }
-
-                // determine if bid was high enough
-                if (chosenItem.highest_bid < parseInt(answer.bid)) {
-                    // bid was high enough, so update db, let the user know, and start over
-                    connection.query(
-                        "UPDATE auctions SET ? WHERE ?",
-                        [
-                            {
-                                highest_bid: answer.bid
-                            },
-                            {
-                                id: chosenItem.id
-                            }
-                        ],
-                        function (error) {
-                            if (error) throw err;
-                            console.log("Bid placed successfully!");
-                            start();
-                        }
-                    );
-                }
-                else {
-                    // bid wasn't high enough, so apologize and start over
-                    console.log("Your bid was too low. Try again...");
-                    start();
-                }
+                    console.log(answer);
+                    connection.end();
+                });
             });
     });
-}
+};
+
+
+
+
+
 
 
